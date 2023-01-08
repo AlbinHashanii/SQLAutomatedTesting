c9e286f50e2a6a553e552064b505396a072bc928
AlbinHashani <94974962+AlbinHashanii@users.noreply.github.com>
2023-01-08 15:28:25 +0100

    Update automatedTesting.py

--- automatedTesting.py
+++ automatedTesting.py
@@ -48,25 +48,25 @@ def clear_clicked():
 
 # Dritarja e GUI
 master = tk.Tk()
-master.title("Automatic SQL Injection Test")
-master.configure(bg='#8dabba')
-master.geometry('450x150')
+master.title("Automated SQL Injection Testing")
+master.configure(bg='#34495e')
+master.geometry('550x200')
 # Madhesia e kufizuar e dritares
 master.resizable(0, 0)
+Font_tuple = ("Courier New", 13)
 
 url = tk.StringVar(master)
-# Label e instruksionit
-tk.Label(master, fg="#1d2122", bg="#8dabba", font=(None, 13),
+tk.Label(master, fg="#000", font=Font_tuple,
          text="Give the url of the website that you want to test:").grid(row=0, column=1, padx=(5, 0), pady=(20, 0))
 # Gjatesia e hapesires per vendojen e url-es
-e1 = tk.Entry(master, width=52, textvariable=url)
-e1.grid(row=1, column=1, padx=(0, 3), pady=(10, 0))
+e1 = tk.Entry(master, width=52, textvariable=url,font=Font_tuple)
+e1.grid(row=1, column=1, padx=(3, 3), pady=(10, 20))
 
 # Butoni CHECK
-tk.Button(master, fg="#ffffff", bg="#253c5a", width=12, font=(None, 13), text="TEST", command=check_injection).grid(
+tk.Button(master, fg="#000", bg="#a6acaf", width=12, font=Font_tuple, text="TEST", command=check_injection).grid(
     row=2, column=1, padx=(0, 190), pady=(15, 0))
 # Butoni CLEAR
-tk.Button(master, fg="#ffffff", bg="#253c5a", width=12, font=(None, 13), text="CLEAR", command=clear_clicked).grid(
+tk.Button(master, fg="#000", bg="#a6acaf", width=12, font=Font_tuple, text="CLEAR", command=clear_clicked).grid(
     row=2, column=1, padx=(190, 0), pady=(15, 0))
 
 master.mainloop()
