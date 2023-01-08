40b54dbba690956f8dd1e21d8fdef291f123899a
Albinnn <94974962+Albinnn@users.noreply.github.com>
2023-01-08 14:36:48 +0100

    Update automatedTesting.py

--- automatedTesting.py
+++ automatedTesting.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+from tkinter import Label, Button, messagebox
+import requests
+import penetrationTesting as p
+import re
+import unittest
+
+regex = re.compile(
+    r'^(?:http|ftp)s?://'
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'localhost|'
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    r'(?::\d+)?'
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+
+
+# Funksioni per skanimin e website
+def give_result(url):
+    if p.scan_sql_injection(url):
+        return 'This website is vulnerable.'
+    else:
+        return 'This website is not vulnerable.'
 
 
 
